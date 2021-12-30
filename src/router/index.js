bb7c64a6eb0efde7a4d963229ce0a55e1a983f63
hongwei.zhw <497354870@qq.com>
2021-12-30 20:57:12 +0800

    修复scss不能使用的问题

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/views/HelloWorld'
 import Home from '@/views/Home'
+import Login from '@/views/Login'
 import Home2 from "../views/Home2";
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/home2',
       name: 'Home2',
       component: Home2
+    },
+    {
+      path: '/login',
+      name: '登录',
+      component: Login
     }
   ]
 })
